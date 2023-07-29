8d265f4b9cbd2ba79484b55ac2ae1de0f0bb3728
harshilquokka <dodgewisdom@gmail.com>
2023-07-29 10:26:56 +0530

    [FIX]Bugs fixed

--- app/src/main/java/com/example/baseandroid/features/main/ui/homepage/ProfileFeedFragment.kt
+++ app/src/main/java/com/example/baseandroid/features/main/ui/homepage/ProfileFeedFragment.kt
@@ -7,13 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.baseandroid.R
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFeedFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFeedFragment : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +16,4 @@ class ProfileFeedFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile_feed, container, false)
     }
-
 }
